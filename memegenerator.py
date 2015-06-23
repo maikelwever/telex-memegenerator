d1952c3e630a0971dc085c97a734e2a921b7509f
Sietse van der Molen <sietse@vdmolen.eu>
2015-06-23 13:26:19 +0200

    whoops

--- memegenerator.py
+++ memegenerator.py
@@ -34,7 +34,7 @@ class MemeGeneratorPlugin(plugin.TelexPlugin):
         "^{prefix}memelist$": "list_memes",
         "^{prefix}argumentinvalid$": "argument_invalid",
         '^{prefix}memesearch (\w+)$': "search_memes",
-        '^{prefix}meme\s*(?P<meme_name>\w+)\s*(?:"(?P<top_text>.+?)")*\s*(?:"(?P<bottom_text>.+?)")*$': "make_meme",
+        '^{prefix}meme\s*(?P<meme_name>\w+)\s*(?:"(?P<top_text>[\d\s\w])")*\s*(?:"(?P<bottom_text>[\d\s\w])")*$': "make_meme",
     }
 
     usage = [
